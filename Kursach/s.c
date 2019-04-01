f62c60ce9206fd764a62a9f687b94855d95ae813
ekaterinasmirnova1 <smirnova190998@mail.ru>
2019-04-01 16:36:47 +0600

    create project
    create project

---
+++ Kursach/s.c
@@ -0,0 +1,26 @@
+#include <windows.h>
+#include <stdio.h>
+
+void constructWindow(int x, int y){
+
+}
+void addVine(int x, int y){
+
+}
+void addPie(int x, int y){
+
+}
+void phyloThread(){
+
+}
+void draw(){
+
+}
+void start(int phyloCount){
+
+}
+void main(){
+    constructWindow(100, 40);
+    start(6);
+    
+}
